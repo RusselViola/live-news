80001dd0e7c7217e3bdddddb80b3439d621d7cc8
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-02-17 18:38:33 -0500

    refactored components to stateless

--- react/src/components/ArticleCarousel.js
+++ react/src/components/ArticleCarousel.js
@@ -1,29 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Carousel, Image } from 'react-bootstrap';
 
-export default class ArticleCarousel extends Component {
-
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    let articles = this.props.articleArray.map(article => {
-      return(
-        <Carousel.Item>
-          <Image src={article.urlToImage} responsive />
-          <Carousel.Caption>
-            <a href={article.url} target="_blank"><h3>{article.title}</h3></a>
-            <p>{article.description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      )
-    })
-    return(
-      <Carousel>
-        {articles}
-      </Carousel>
+const ArticleCarousel = props => {
+  let articles = props.articleArray.map(article => {
+    return (
+      <Carousel.Item>
+        <Image src={article.urlToImage} responsive />
+        <Carousel.Caption>
+          <a href={article.url} target="_blank"><h3>{article.title}</h3></a>
+          <p>{article.description}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
     )
-  }
+  })
+  return (
+    <Carousel>
+      {articles}
+    </Carousel>
+  );
+};
 
-}
+export default ArticleCarousel;
